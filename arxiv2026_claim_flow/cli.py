b384146df68c2b805a7170b8d165e08e87e5a5ab
akatief <44204455+akatief@users.noreply.github.com>
2026-01-09 15:54:56 +0000

    ✅ Ready to clone and code.

--- arxiv2026_claim_flow/cli.py
+++ arxiv2026_claim_flow/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for ukp_project_template project.
+"""CLI interface for arxiv2026_claim_flow project.
 
 Be creative! do whatever you want!
 
@@ -13,7 +13,7 @@ from .subpackage import SubPackageClass
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m ukp_project_template` and `$ ukp_project_template `.
+    `python -m arxiv2026_claim_flow` and `$ arxiv2026_claim_flow `.
 
     This is your program's entry point.
 
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,6 @@
 # Tests are defined here
-from ukp_project_template import BaseClass
-from ukp_project_template.subpackage import SubPackageClass
+from arxiv2026_claim_flow import BaseClass
+from arxiv2026_claim_flow.subpackage import SubPackageClass
 
 def test_template():
     assert True
